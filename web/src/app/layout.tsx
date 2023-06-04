6e47ff01be55031df0082a7b018ec7721afb855f
heitorlisboa <heitor01101000@gmail.com>
2023-06-03 22:24:23 -0300

    refactor(web): move some content from the home page to the application layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,13 @@ import { Roboto_Flex, Bai_Jamjuree } from 'next/font/google';
 
 import './globals.css';
 
+import { getSession } from '@/lib/auth';
+
+import { Profile } from '@/components/Profile';
+import { SignIn } from '@/components/SignIn';
+import { Hero } from '@/components/Hero';
+import { Copyright } from '@/components/Copyright';
+
 const robotoFlex = Roboto_Flex({
   subsets: ['latin'],
   variable: '--font-roboto-flex',
@@ -23,12 +30,36 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { isUserAuthed, userInfo } = getSession();
+
   return (
     <html lang="en">
       <body
         className={`${robotoFlex.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100 antialiased`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-[50%] bg-purple-700/50 blur-[194px]" />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isUserAuthed ? (
+              <Profile name={userInfo.name} avatarUrl={userInfo.avatarUrl} />
+            ) : (
+              <SignIn />
+            )}
+            <Hero />
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,37 +1,5 @@
-import { getSession } from '@/lib/auth';
-
-import { SignIn } from '@/components/SignIn';
-import { Hero } from '@/components/Hero';
-import { Copyright } from '@/components/Copyright';
 import { EmptyMemories } from '@/components/EmptyMemories';
-import { Profile } from '@/components/Profile';
 
 export default function Home() {
-  const { isUserAuthed, userInfo } = getSession();
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(/bg-stars.svg)] bg-cover px-28 py-16">
-        {/* Blur */}
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-[50%] bg-purple-700/50 blur-[194px]" />
-
-        {/* Stripes */}
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-
-        {isUserAuthed ? (
-          <Profile name={userInfo.name} avatarUrl={userInfo.avatarUrl} />
-        ) : (
-          <SignIn />
-        )}
-        <Hero />
-        <Copyright />
-      </div>
-
-      {/* Right */}
-      <div className="flex flex-col bg-[url(/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  );
+  return <EmptyMemories />;
 }
